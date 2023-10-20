e8f1449736cb712fcdf0856be3bedce60cc207bf
Erik05Master <45329312+Erik05Master@users.noreply.github.com>
2023-10-20 23:05:12 +0200

    Update python_installer.sh 3.11.5 to 3.12.0

--- python_installer.sh
+++ python_installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Installer for Python3.11 and pip
+#Installer for Python3.12 and pip
 
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
@@ -13,11 +13,11 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
 
-wget https://www.python.org/ftp/python/3.11.5/Python-3.11.5.tgz
+wget https://www.python.org/ftp/python/3.12.0/Python-3.12.0.tgz
 
-tar -xf Python-3.11.*.tgz
+tar -xf Python-3.12.*.tgz
 
-cd Python-3.11.*/
+cd Python-3.12.*/
 
 ./configure --enable-optimizations
 
@@ -33,6 +33,6 @@ python3.11 get-pip.py
 
 python3.11 --version
 
-rm -r Python-3.11.*/ Python-3.11.*.tgz get-pip.py
+rm -r Python-3.12.*/ Python-3.12.*.tgz get-pip.py
 
 echo -e "\n\n\nPython3.11 and pip has been installed.\n\n\n"
